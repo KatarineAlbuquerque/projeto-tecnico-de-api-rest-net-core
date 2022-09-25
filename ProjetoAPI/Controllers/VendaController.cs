eb2f1b9e45e6a70103fc479f36dbd122faa769fc
Katarine Albuquerque <kba.2879@gmail.com>
2022-09-25 14:05:56 -0300

    Criação da Classe VendaController

---
+++ ProjetoAPI/Controllers/VendaController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoAPI.Context;
+
+namespace ProjetoAPI.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class VendaController : ControllerBase
+    {
+        private VendaContext _context;
+
+        [HttpPost("RegistrarVenda")]
+        public IActionResult RegistrarVenda()
+        {
+            _context = new VendaContext();
+
+            var pedido = _context.AdicionarVenda();
+
+            var dados = new
+            {
+                pedido = pedido,
+            };
+
+            return Ok(dados);
+        }
+
+        [HttpGet("BuscarVenda/{id}")]
+        public IActionResult BuscarVenda(int id)
+        {
+            _context = new VendaContext();
+
+            var pedidos = _context.BuscarVendaPorId(id);
+
+            var dados = new
+            {
+                pedido = pedidos,
+            };
+
+            return Ok(dados);
+        }
+
+        [HttpPut("AtualizarVenda/{id}")]
+        public IActionResult AtualizarVenda(int id)
+        {
+            _context = new VendaContext();
+
+            var pedido = _context.AtualizarVendaPorId(id);
+
+            var dados = new
+            {
+                pedido = pedido,
+            };
+
+            return Ok(dados);
+        }
+    }
+}
