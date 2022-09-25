f5c8b0d008aa63846ab568f17353165708795393
Katarine Albuquerque <kba.2879@gmail.com>
2022-09-25 13:51:01 -0300

    Criação da Classe Pessoa

---
+++ ProjetoAPI/Entities/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿namespace ProjetoAPI.Entities
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+
+        public Pessoa()
+        {
+
+        }
+
+        public Pessoa(int id, string cpf, string nome, string email, string telefone)
+        {
+            Id = id;
+            Cpf = cpf;
+            Nome = nome;
+            Email = email;
+            Telefone = telefone;
+        }
+    }
+}
