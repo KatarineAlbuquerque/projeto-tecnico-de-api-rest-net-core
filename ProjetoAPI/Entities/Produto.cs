21fa95514ad39326f54c46c7ea0086a75834be85
Katarine Albuquerque <kba.2879@gmail.com>
2022-09-25 13:53:29 -0300

    Criação da Classe Produto

---
+++ ProjetoAPI/Entities/Produto.cs
@@ -0,0 +1,23 @@
+﻿namespace ProjetoAPI.Entities
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Preco { get; set; }
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(int id, string nome, int quantidade, decimal preco)
+        {
+            Id = id;
+            Nome = nome;
+            Quantidade = quantidade;
+            Preco = preco;
+        }
+    }
+}
