ac081ce63a758b2aaeb8d9e3bed130c2d7390537
Katarine Albuquerque <kba.2879@gmail.com>
2022-09-25 13:55:11 -0300

    Criação da Classe Pedido

---
+++ ProjetoAPI/Entities/Pedido.cs
@@ -0,0 +1,31 @@
+﻿namespace ProjetoAPI.Entities
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public int IdPedido { get; set; }
+        public Pessoa Vendedor { get; set; }
+        public DateTime Data { get; set; }
+        public List<Produto> Produtos { get; set; }
+        public string Status { get; set; }
+
+        public Pedido()
+        {
+
+        }
+
+        public Pedido(int id)
+        {
+            Id = id;
+        }
+
+        public Pedido(int id, int idPedido, Pessoa vendedor, DateTime data, string status)
+        {
+            Id = id;
+            IdPedido = idPedido;
+            Vendedor = vendedor;
+            Data = data;
+            Status = status;
+        }
+    }
+}
